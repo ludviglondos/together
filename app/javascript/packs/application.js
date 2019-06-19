5ca3c60fd5d8f63dddfb1f87f7810fc867c90d75
Johan Hakansson <johan.hakansson1@ikea.com>
2019-06-19 14:14:30 +0200

    Banner created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,6 @@ import "../plugins/flatpickr"
 $(document).ready(function() {
     $(".cards_expanded").hide();
     $(".dot_container").click(function(){
-      $(".cards_expanded").slideToggle("slow");
+      $(".cards_expanded").slideToggle("swing");
     });
 });
