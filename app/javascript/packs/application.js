db03769740aaf5186316b08be930d3f1149a5e62
Johan Hakansson <johan.hakansson1@ikea.com>
2019-06-19 10:37:17 +0200

    Added JQuery for button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,11 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+
+
+
+$(document).ready(function() {
+    $(".cards_expanded").hide();
+    $(".dot_container").click(function(){
+      $(".cards_expanded").slideToggle("slow");
+    });
+});
